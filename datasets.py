f25b4d3bec4f43831a963d81502381705e55bbc7
WangZihao121 <205223196@qq.com>
2025-10-11 22:28:57 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,16 +1,17 @@
-import h5py
+ import h5py
 import numpy as np
 from torch.utils.data import Dataset
 
 
 class TrainDataset(Dataset):
     def __init__(self, h5_file):
-        super(TrainDataset, self).__init__()
+        super(TrainDataset, self).__init__()  #super函数：使这段代码运行了一遍TrainDataset的父类的init函数
         self.h5_file = h5_file
 
     def __getitem__(self, idx):
-        with h5py.File(self.h5_file, 'r') as f:
-            return np.expand_dims(f['lr'][idx] / 255., 0), np.expand_dims(f['hr'][idx] / 255., 0)
+        with h5py.File(self.h5_file, 'r') as f:        #r表示只读模式，把打开的文件命名为f，with的生命周期结束后会自动关闭文件
+            return np.expand_dims(f['lr'][idx] / 255., 0), np.expand_dims(f['hr'][idx] / 255., 0)          # f['lr'][idx] / 255是为了归一化，把数变为0~1，方便训练
+            
 
     def __len__(self):
         with h5py.File(self.h5_file, 'r') as f:
