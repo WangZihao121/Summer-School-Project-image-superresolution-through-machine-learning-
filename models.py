b724a08ef0916d6ed4265e7d89e5322f92c9db48
WangZihao121 <205223196@qq.com>
2025-10-13 20:27:52 +0800

    Update models.py

--- models.py
+++ models.py
@@ -8,15 +8,15 @@ class FSRCNN(nn.Module):
 
 
 
-        # 特征提取层：使用 5x5 卷积提取图像初始特征，通道数从 num_channels 转为 d
+        
         self.first_part = nn.Sequential(
-            nn.Conv2d(num_channels, d, kernel_size=5, padding=5//2),
+            nn.Conv2d(num_channels, d, kernel_size=5, padding=5//2),               
             nn.PReLU(d)
-        )        #这行代码创建了一个顺序容器 nn.Sequential，它是nn.Module的一个子类，你可以把它理解成一个“流水线”，数据会依次流过每一层。
-                 #self.first_part是nn.Sequential这个类的实例化。
+        )        #这行代码创建了一个顺序容器 nn.Sequential，它是nn.Module的一个子类，你可以把它理解成一个“流水线”，如果给它一个数据，那数据会依次流过每一层。
+                 #self.first_part是nn.Sequential这个类的实例化，成了一个有两个层的神经网络。
                  #此处不能理解为nn.Sequential这个类继承了nn.Conv2d和nn.PReLU这两个类
-        # 特征收缩与非线性映射层：先压缩通道数，再通过多个 3x3 卷积进行深度映射
-
+                 #nn.Conv2d(num_channels, d, kernel_size=5, padding=5//2)   =   nn.Conv2d(num_channels, d, 5, 5//2) 这是个卷积层
+        
 
         
         self.mid_part = [nn.Conv2d(d, s, kernel_size=1), nn.PReLU(s)]  # 通道压缩。放了两个类在列表中
@@ -47,10 +47,12 @@ class FSRCNN(nn.Module):
     # 权重初始化函数：使用 He 初始化方法对卷积层权重进行初始化
     def _initialize_weights(self):
         for m in self.first_part:
-            if isinstance(m, nn.Conv2d):
+            #虽然self.first_part不是一个列表类型数据，但是nn.Sequential 实现了 __getitem__ 方法，使其支持索引。这个循环其实是在遍历self.first_part神经网络中的每个层
+            if isinstance(m, nn.Conv2d):    #isinstance函数：判断m是不是nn.Conv2d类型或者其子类
                 nn.init.normal_(m.weight.data, mean=0.0,
-                                std=math.sqrt(2/(m.out_channels * m.weight.data[0][0].numel())))
-                nn.init.zeros_(m.bias.data)
+                                std=math.sqrt(2/(m.out_channels * m.weight.data[0][0].numel())))    #通过正态分布随机抽样生成卷积核数据
+                # 当m是卷积层的时候，m.weight是卷积核的可训练版本，m.weight.data是卷积核本身的数值
+                nn.init.zeros_(m.bias.data) # m.bias.data是卷积层的偏置参数的数值部分，nn.init.zeros_将其设为0。在卷积运算中：输出 = 卷积(输入 × 权重) + 偏置
         for m in self.mid_part:
             if isinstance(m, nn.Conv2d):
                 nn.init.normal_(m.weight.data, mean=0.0,
