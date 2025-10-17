772473a047c298ef3346789c177a8d0aa01e160c
WangZihao121 <205223196@qq.com>
2025-10-17 12:54:11 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -10,7 +10,7 @@ class TrainDataset(Dataset):
 
     def __getitem__(self, idx):
         with h5py.File(self.h5_file, 'r') as f:        #r表示只读模式，把打开的文件命名为f，with的生命周期结束后会自动关闭文件
-            return np.expand_dims(f['lr'][idx] / 255., 0), np.expand_dims(f['hr'][idx] / 255., 0)          # f['lr'][idx] / 255是为了归一化，把数变为0~1，方便训练
+            return np.expand_dims(f['lr'][idx] / 255., 0), np.expand_dims(f['hr'][idx] / 255., 0)   #np.expand_dims（numpy数组，在哪个位置加入新维度）返回值依旧是numpy数组       # f['lr'][idx] / 255是为了归一化，把数变为0~1，方便训练
             
 
     def __len__(self):
